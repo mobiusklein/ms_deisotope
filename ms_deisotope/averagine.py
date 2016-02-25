09aadf679052ee0b6aa3bf9ad56803b068f3ea5d
Joshua Klein <mobiusklein@gmail.com>
2016-02-24 20:54:05 -0500

    Added truncation to Averagine

--- ms_deisotope/averagine.py
+++ ms_deisotope/averagine.py
@@ -15,9 +15,16 @@ class Averagine(object):
             scaled[elem] = count * scale
         return scaled
 
-    def isotopic_cluster(self, mz, charge=1, charge_carrier=PROTON, truncate_after=1e-5):
+    def isotopic_cluster(self, mz, charge=1, charge_carrier=PROTON, truncate_after=0.9):
         composition = self.scale(mz, charge, charge_carrier)
-        return [p for p in isotopic_variants(composition, charge=charge) if p.intensity > truncate_after]
+        cumsum = 0
+        result = []
+        for peak in isotopic_variants(composition, charge=charge):
+            cumsum += peak.intensity
+            result.append(peak)
+            if cumsum >= truncate_after:
+                break
+        return result
 
 
 peptide = Averagine({"C": 4.9384, "H": 7.7583, "N": 1.3577, "O": 1.4773, "S": 0.0417})
