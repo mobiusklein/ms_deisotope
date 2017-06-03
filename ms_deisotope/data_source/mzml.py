bfb2c4a793383cbb29d81096ec43ff9957b8c4c9
Joshua Klein <mobiusklein@gmail.com>
2017-06-03 17:01:36 -0400

    Added documentation for MzMLDataInterface

--- ms_deisotope/data_source/mzml.py
+++ ms_deisotope/data_source/mzml.py
@@ -12,12 +12,42 @@ class MzMLDataInterface(ScanDataSource):
     :class:`ScanDataSource` for mzML files. Not intended for direct instantiation.
     """
     def _scan_arrays(self, scan):
+        """Returns raw data arrays for m/z and intensity
+
+        Parameters
+        ----------
+        scan : Mapping
+            The underlying scan information storage,
+            usually a `dict`
+
+        Returns
+        -------
+        mz: np.array
+            An array of m/z values for this scan
+        intensity: np.array
+            An array of intensity values for this scan
+        """
         try:
             return scan['m/z array'], scan["intensity array"]
         except KeyError:
             return np.array([]), np.array([])
 
     def _precursor_information(self, scan):
+        """Returns information about the precursor ion,
+        if any, that this scan was derived form.
+
+        Returns `None` if this scan has no precursor ion
+
+        Parameters
+        ----------
+        scan : Mapping
+            The underlying scan information storage,
+            usually a `dict`
+
+        Returns
+        -------
+        PrecursorInformation
+        """
         pinfo_dict = scan["precursorList"]['precursor'][0]["selectedIonList"]['selectedIon'][0]
         try:
             precursor_scan_id = scan["precursorList"]['precursor'][0]['spectrumRef']
@@ -32,38 +62,155 @@ class MzMLDataInterface(ScanDataSource):
             product_scan_id=self._scan_id(scan))
         return pinfo
 
-    # Optional
     def _scan_title(self, scan):
+        """Returns a verbose name for this scan, if one
+        were stored in the file. Usually includes both the
+        scan's id string, as well as information about the
+        original file and format.
+
+        Parameters
+        ----------
+        scan : Mapping
+            The underlying scan information storage,
+            usually a `dict`
+
+        Returns
+        -------
+        str
+        """
         try:
             return scan["spectrum title"]
         except KeyError:
             return scan["id"]
 
     def _scan_id(self, scan):
+        """Returns the scan's id string, a unique
+        identifier for this scan in the context of
+        the data file it is recordered in
+
+        Parameters
+        ----------
+        scan : Mapping
+            The underlying scan information storage,
+            usually a `dict`
+
+        Returns
+        -------
+        str
+        """
         return scan["id"]
 
     def _scan_index(self, scan):
+        """Returns the base 0 offset from the start
+        of the data file in number of scans to reach
+        this scan.
+
+        If the original format does not natively include
+        an index value, this value may be computed from
+        the byte offset index.
+
+        Parameters
+        ----------
+        scan : Mapping
+            The underlying scan information storage,
+            usually a `dict`
+
+        Returns
+        -------
+        int
+        """
         return scan['index']
 
     def _ms_level(self, scan):
-        return scan['ms level']
+        """Returns the degree of exponential fragmentation
+        used to produce this scan.
+
+        1 refers to a survey scan of unfragmented ions, 2
+        refers to a tandem scan derived from an ms level 1
+        ion, and so on.
+
+        Parameters
+        ----------
+        scan : Mapping
+            The underlying scan information storage,
+            usually a `dict`
+
+        Returns
+        -------
+        int
+        """
+        return int(scan['ms level'])
 
     def _scan_time(self, scan):
+        """Returns the time in minutes from the start of data
+        acquisition to when this scan was acquired.
+
+        Parameters
+        ----------
+        scan : Mapping
+            The underlying scan information storage,
+            usually a `dict`
+
+        Returns
+        -------
+        float
+        """
         try:
             return scan['scanList']['scan'][0]['scan start time']
         except KeyError:
             return None
 
     def _is_profile(self, scan):
+        """Returns whether the scan contains profile data (`True`)
+        or centroided data (`False`).
+        
+        Parameters
+        ----------
+        scan : Mapping
+            The underlying scan information storage,
+            usually a `dict`
+        
+        Returns
+        -------
+        bool
+        """
         return "profile spectrum" in scan
 
     def _polarity(self, scan):
+        """Returns whether this scan was acquired in positive mode (+1)
+        or negative mode (-1).
+        
+        Parameters
+        ----------
+        scan : Mapping
+            The underlying scan information storage,
+            usually a `dict`
+        
+        Returns
+        -------
+        int
+        """
         if "positive scan" in scan:
             return 1
         elif "negative scan" in scan:
             return -1
 
     def _activation(self, scan):
+        """Returns information about the activation method used to
+        produce this scan, if any.
+
+        Returns `None` for MS1 scans
+
+        Parameters
+        ----------
+        scan : Mapping
+            The underlying scan information storage,
+            usually a `dict`
+
+        Returns
+        -------
+        ActivationInformation
+        """
         try:
             struct = dict(scan['precursorList']['precursor'][0]['activation'])
             activation = None
@@ -106,9 +253,6 @@ class MzMLLoader(MzMLDataInterface, XMLReaderBase):
         self._scan_cache = WeakValueDictionary()
         self._use_index = use_index
 
-    def __reduce__(self):
-        return self.__class__, (self.source_file, self._use_index)
-
     def file_description(self):
         return _find_section(self._source, "fileDescription")
 
@@ -138,6 +282,3 @@ class MzMLLoader(MzMLDataInterface, XMLReaderBase):
             start = 0
         for key in keys[start:]:
             yield scan_source.get_by_id(key)
-
-
-# PyteomicsMzMLLoader = MzMLLoader
