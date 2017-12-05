a857b818da80ecf3b32ea20a9da3548b41f5562c
Joshua Klein <mobiusklein@gmail.com>
2017-12-04 21:14:20 -0500

    replace extended index entry dictionaries with classes to provide graceful defaults for newer schemata

--- ms_deisotope/feature_map/scan_index.py
+++ ms_deisotope/feature_map/scan_index.py
@@ -1,9 +1,71 @@
 import json
-import warnings
 
 from collections import OrderedDict
 
 from ms_deisotope.data_source.common import PrecursorInformation, ChargeNotProvided
+from ms_deisotope.utils import Base
+
+
+class MSRecordBase(Base):
+    def __init__(self, scan_time, drift_time=None, **kwargs):
+        self.scan_time = scan_time
+        self.drift_time = drift_time
+        self._extra_keys = kwargs.keys()
+        for key, value in kwargs.items():
+            setattr(self, key, value)
+
+    def __getitem__(self, key):
+        return getattr(self, key)
+
+    def to_dict(self):
+        package = {
+            "scan_time": self.scan_time,
+        }
+        if self.drift_time is not None:
+            package['drift_time'] = self.drift_time
+        for key in self._extra_keys:
+            package[key] = self[key]
+        return package
+
+
+class MS1Record(MSRecordBase):
+    def __init__(self, scan_time=None, product_scan_ids=None, msms_peaks=None, drift_time=None, **kwargs):
+        super(MS1Record, self).__init__(scan_time, drift_time, **kwargs)
+        self.product_scan_ids = product_scan_ids or []
+        self.msms_peaks = msms_peaks or []
+
+    def to_dict(self):
+        package = super(MS1Record, self).to_dict()
+        package['product_scan_ids'] = self.product_scan_ids
+        package['msms_peaks'] = self.msms_peaks
+        return package
+
+
+class MSnRecord(MSRecordBase):
+    def __init__(self, scan_time=None, neutral_mass=None, mz=None, intensity=None, charge=None,
+                 precursor_scan_id=None, product_scan_id=None, defaulted=None, orphan=None,
+                 drift_time=None, **kwargs):
+        super(MSnRecord, self).__init__(scan_time, drift_time, **kwargs)
+        self.neutral_mass = neutral_mass
+        self.mz = mz
+        self.intensity = intensity
+        self.charge = charge
+        self.precursor_scan_id = precursor_scan_id
+        self.product_scan_id = product_scan_id
+        self.defaulted = defaulted
+        self.orphan = orphan
+
+    def to_dict(self):
+        package = super(MSnRecord, self).to_dict()
+        package['neutral_mass'] = self.neutral_mass
+        package['mz'] = self.mz
+        package['intensity'] = self.intensity
+        package['charge'] = self.charge
+        package['precursor_scan_id'] = self.precursor_scan_id
+        package['product_scan_id'] = self.product_scan_id
+        package['defaulted'] = self.defaulted
+        package['orphan'] = self.orphan
+        return package
 
 
 class ExtendedScanIndex(object):
@@ -49,7 +111,7 @@ class ExtendedScanIndex(object):
             if product.has_ion_mobility():
                 package['drift_time'] = product.drift_time
         else:
-            charge = precursor_information.extracted_charge
+            charge = precursor_information.charge
             if charge == ChargeNotProvided:
                 charge = 'ChargeNotProvided'
             package = {
@@ -80,14 +142,14 @@ class ExtendedScanIndex(object):
         }
         if bunch.precursor.has_ion_mobility():
             package['drift_time'] = bunch.precursor.drift_time
-        self.ms1_ids[bunch.precursor.id] = package
+        self.ms1_ids[bunch.precursor.id] = MS1Record(**package)
         for product in bunch.products:
-            self.msn_ids[product.id] = self._package_precursor_information(product)
+            self.msn_ids[product.id] = MSnRecord(**self._package_precursor_information(product))
 
     def serialize(self, handle):
         mapping = {
-            "ms1_ids": list(self.ms1_ids.items()),
-            "msn_ids": list(self.msn_ids.items()),
+            "ms1_ids": [(k, v.to_dict()) for k, v in self.ms1_ids.items()],
+            "msn_ids": [(k, v.to_dict()) for k, v in self.msn_ids.items()],
             "schema_version": self.schema_version,
         }
         json.dump(mapping, handle)
@@ -105,6 +167,10 @@ class ExtendedScanIndex(object):
     @classmethod
     def deserialize(cls, handle):
         mapping = json.load(handle)
+        ms1_ids = mapping.get("ms1_ids", [])
+        mapping['ms1_ids'] = [(k, MS1Record(**v)) for k, v in ms1_ids]
+        msn_ids = mapping.get("msn_ids", [])
+        mapping['msn_ids'] = [(k, MSnRecord(**v)) for k, v in msn_ids]
         return cls(**mapping)
 
     def get_precursor_information(self, bind=None):
