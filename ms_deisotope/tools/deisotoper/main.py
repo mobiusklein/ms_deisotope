a92ca8f7ee55198e01f0b5e13541a161de4fc916
Joshua Klein <mobiusklein@gmail.com>
2019-01-16 11:08:03 -0500

    feature: Added `describe` command to ms-index

--- ms_deisotope/tools/deisotoper/main.py
+++ ms_deisotope/tools/deisotoper/main.py
@@ -6,65 +6,14 @@ import click
 
 import ms_peak_picker
 
-from brainpy import periodic_table
 from ms_peak_picker.scan_filter import parse as parse_filter
 
 import ms_deisotope
 from ms_deisotope import MSFileLoader
 from ms_deisotope.data_source import RandomAccessScanSource
 
+from ms_deisotope.tools.utils import processes_option, AveragineParamType
 from ms_deisotope.tools.deisotoper import workflow
-from ms_deisotope.averagine import (
-    Averagine, glycan as n_glycan_averagine, permethylated_glycan,
-    peptide, glycopeptide, heparin, heparan_sulfate)
-
-
-def validate_element(element):
-    valid = element in periodic_table
-    if not valid:
-        raise click.Abort("%r is not a valid element" % element)
-    return valid
-
-
-def parse_averagine_formula(formula):
-    if isinstance(formula, Averagine):
-        return formula
-    return Averagine({k: float(v) for k, v in re.findall(r"([A-Z][a-z]*)([0-9\.]*)", formula)
-                      if float(v or 0) > 0 and validate_element(k)})
-
-
-averagines = {
-    'glycan': n_glycan_averagine,
-    'permethylated-glycan': permethylated_glycan,
-    'peptide': peptide,
-    'glycopeptide': glycopeptide,
-    'heparin': heparin,
-    "heparan-sulfate": heparan_sulfate
-}
-
-
-def validate_averagine(averagine_string):
-    if isinstance(averagine_string, Averagine):
-        return averagine_string
-    if averagine_string in averagines:
-        return averagines[averagine_string]
-    else:
-        return parse_averagine_formula(averagine_string)
-
-
-class AveragineParamType(click.types.StringParamType):
-    name = "MODEL"
-
-    models = averagines
-
-    def convert(self, value, param, ctx):
-        return validate_averagine(value)
-
-    def get_metavar(self, param):
-        return '[%s]' % '|'.join(sorted(averagines.keys()))
-
-    def get_missing_message(self, param):
-        return 'Choose from %s, or provide a formula.' % ', '.join(self.choices)
 
 
 def check_random_access(loader, start_time, end_time):
@@ -141,10 +90,7 @@ def check_if_profile(loader):
               help="Number of missing peaks to permit before an isotopic fit is discarded")
 @click.option("-mn", "--msn-missed-peaks", type=int, default=1,
               help="Number of missing peaks to permit before an isotopic fit is discarded in an MSn scan")
-@click.option(
-    "-p", "--processes", 'processes', type=click.IntRange(1, multiprocessing.cpu_count()),
-    default=min(multiprocessing.cpu_count(), 4), help=('Number of worker processes to use. Defaults to 4 '
-                                                       'or the number of CPUs, whichever is lower'))
+@processes_option
 @click.option("-b", "--background-reduction", type=float, default=0., help=(
               "Background reduction factor. Larger values more aggresively remove low abundance"
               " signal in MS1 scans."))
