df44f7b2ad2a1e32e5b6ce5f5dc760adb3f34414
Joshua Klein <mobiusklein@gmail.com>
2019-04-29 13:06:55 -0400

    Feature:Implement basic QC namespace. More to come

---
+++ ms_deisotope/qc/__init__.py
@@ -0,0 +1,8 @@
+"""A collection of methods for determining whether a given spectrum is
+of high quality (likely to produce a high quality interpretation)
+"""
+from .heuristic import xrea
+
+__all__ = [
+    "xrea"
+]
