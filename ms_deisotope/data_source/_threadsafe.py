961662162aef1b9f81083db65b451bb33c204889
Joshua Klein <mobiusklein@gmail.com>
2020-02-28 08:38:12 -0500

    Studying thread safety of iterators and files

--- ms_deisotope/data_source/_threadsafe.py
+++ ms_deisotope/data_source/_threadsafe.py
@@ -1,8 +1,56 @@
 import threading
+import logging
 
 from .scan.scan import Scan
 from .scan.loader import RandomAccessScanSource
 from .infer_type import MSFileLoader
+from ._compression import get_opener
+
+
+logger = logging.getLogger(__name__)
+logger.addHandler(logging.NullHandler())
+
+
+class PerThreadFileHandle(object):
+    def __init__(self, path, mode='rb', encoding=None, opener=None):
+        if opener is None:
+            opener = get_opener
+        self.local = threading.local()
+        self._path = path
+        self._mode = mode
+        self._encoding = encoding
+        self._opener = opener
+
+    @property
+    def file(self):
+        try:
+            return self.local.file
+        except AttributeError:
+            return self._open()
+
+    def __len__(self):
+        return len(self.file)
+
+    def __iter__(self):
+        return iter(self.file)
+
+    def __getitem__(self, i):
+        return self.file[i]
+
+    def __reduce__(self):
+        return self.__class__, (self._path, self._mode, self._encoding, self._opener)
+
+    def _open(self):
+        self.local.file = self._opener(self._path)
+        logger.debug("Opening %r on Thread %r", self._path,
+                     threading.current_thread())
+        return self.local.file
+
+    def __getattr__(self, attrib):
+        if attrib in ('local', '_path', '_mode', '_encoding', '_opener'):
+            raise AttributeError(attrib)
+        return object.__getattribute__(self.file, attrib)
+
 
 
 class ThreadsafeScanSource(object):
