ad5c98776d9f69278e752ab27aa8ecc3fedba75c
Joshua Klein <mobiusklein@gmail.com>
2022-01-13 23:23:20 -0500

    Update hooks

--- pyinstaller_hooks/hook-ms_deisotope.py
+++ pyinstaller_hooks/hook-ms_deisotope.py
@@ -1,4 +1,7 @@
 
-from PyInstaller.utils.hooks import collect_submodules
+from PyInstaller.utils.hooks import collect_submodules, collect_data_files
 
 hiddenimports = collect_submodules("ms_deisotope._c")
+
+datas = list(filter(lambda x: "test_data" not in x[1] and not x[0].endswith(
+    '.c') and not x[0].endswith('.html') and not x[0].endswith('.pyx'), collect_data_files("ms_deisotope")))
