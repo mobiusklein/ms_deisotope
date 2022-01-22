d0b26ea42ca7bf54d5593cfdc857960f50cbfdd7
Joshua Klein <mobiusklein@gmail.com>
2022-01-22 14:31:03 -0500

    Add better gap handling

--- examples/waters_cyclic_deconvolute.py
+++ examples/waters_cyclic_deconvolute.py
@@ -8,13 +8,15 @@ from itertools import chain
 import numpy as np
 
 import ms_deisotope
-from ms_deisotope.feature_map import map_viz
 from ms_deisotope.peak_dependency_network.intervals import Interval, IntervalTreeNode
 from ms_deisotope.peak_set import IonMobilityDeconvolutedPeak, DeconvolutedPeakSet
 from ms_deisotope.data_source.scan import ProcessedScan, PrecursorInformation, ScanBunch
 from ms_deisotope.data_source.scan.scan_iterator import MSEIterator
 from ms_deisotope.output.mzml import MzMLSerializer, IonMobilityAware3DMzMLSerializer, ProcessedGeneric3DIonMobilityFrameSource
 from ms_deisotope.data_source.scan.mobility_frame import IonMobilityFrame, Generic3DIonMobilityFrameSource
+
+from ms_deisotope.tools.utils import register_debug_hook
+
 
 import click
 
@@ -25,10 +27,17 @@ logger.addHandler(logging.NullHandler())
 
 def make_iterator(reader, start_index, stop_index=float('inf'), low_energy_function=1, lock_mass_function=3):
     iterator = MSEIterator(
-        reader.start_from_frame(index=start_index, require_ms1=False, grouped=False),
+        reader.start_from_frame(
+            index=start_index, require_ms1=False, grouped=False),
         lambda x: x, low_energy_function, lock_mass_function)
     for bunch in iterator:
-        if bunch.precursor.index >= stop_index:
+        if bunch.precursor:
+            i = bunch.precursor.index
+        elif bunch.products:
+            i = bunch.products[0].index
+        else:
+            i = 0
+        if i >= stop_index:
             break
         yield bunch
 
@@ -145,7 +154,8 @@ def frame_pair_to_scan_bunch(parent, child, prec_prod_rels, scan_index):
 
 class IonMobilityAwareMzMLSerializer(MzMLSerializer):
     def _prepare_extra_arrays(self, scan, **kwargs):
-        extra_arrays = super(IonMobilityAwareMzMLSerializer, self)._prepare_extra_arrays(scan, **kwargs)
+        extra_arrays = super(IonMobilityAwareMzMLSerializer,
+                             self)._prepare_extra_arrays(scan, **kwargs)
         if scan.deconvoluted_peak_set is not None:
             # This is sensitive to units used? Shouldn't there be a unit key?
             # Waters uses milliseconds
@@ -153,7 +163,6 @@ class IonMobilityAwareMzMLSerializer(MzMLSerializer):
                 p.drift_time for p in scan.deconvoluted_peak_set
             ]))
         return extra_arrays
-
 
 
 averagine_map = {
@@ -165,6 +174,7 @@ averagine_map = {
     "permethylated_glycan": ms_deisotope.permethylated_glycan,
 }
 
+
 @click.command("cyclic_deconvolute")
 @click.argument("input_path", type=click.Path())
 @click.argument("output_path", type=click.Path(writable=True))
@@ -173,33 +183,37 @@ averagine_map = {
 @click.option("-e", "--end-time", type=float, help="The time to stop processing cycles at", default=None)
 @click.option("-a", "--averagine", type=click.Choice(list(averagine_map)), default='glycopeptide',
               help='The isotopic model to use. Defaults to the glycopeptide averagine.')
-@click.option("-i", "--minimum-intensity", type=float, help="The minimum intensity to accept a peak")
+@click.option("-i", "--minimum-intensity", type=float, default=10.0, help="The minimum intensity to accept a peak")
 @click.option("-n", "--no-product-splitting", is_flag=True, default=False,
               help=(
                   'If true, do not split high energy frames into pseudo-MS2 spectra,'
                   ' creating continuous ion mobility features'))
+@click.option("-k", "--lockmass-function", type=int, default=3, help="The number of the lock mass function. For normal low-high MSE this is 3.")
 def main(input_path, output_path, lockmass_config, start_time=0, end_time=None, averagine='glycopeptide',
-         minimum_intensity=10.0, no_product_splitting=False):
+         minimum_intensity=10.0, no_product_splitting=False, lockmass_function=3):
     logging.basicConfig(
         level="INFO", format='%(asctime)s %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p',
         filename="cyclic_deconvolute_%s_%s.log" % (os.path.basename(input_path).rsplit(".", 1)[0], start_time))
     logging.getLogger().addHandler(logging.StreamHandler(sys.stderr))
     input_path = str(input_path)
-    is_mzml = input_path.lower().endswith("mzml") or input_path.lower().endswith(".mzml.gz")
+    is_mzml = input_path.lower().endswith(
+        "mzml") or input_path.lower().endswith(".mzml.gz")
 
     reader = ms_deisotope.MSFileLoader(
         input_path, lockmass_config=lockmass_config)
 
-
     if no_product_splitting:
-        writer = IonMobilityAware3DMzMLSerializer(open(output_path, 'wb'), len(reader))
+        writer = IonMobilityAware3DMzMLSerializer(
+            open(output_path, 'wb'), len(reader))
     else:
-        writer = IonMobilityAwareMzMLSerializer(open(output_path, 'wb'), len(reader))
+        writer = IonMobilityAwareMzMLSerializer(
+            open(output_path, 'wb'), len(reader))
 
     if is_mzml:
         reader = Generic3DIonMobilityFrameSource(reader)
 
-    averagine_model: ms_deisotope.AveragineCache = ms_deisotope.AveragineCache(averagine_map[averagine])
+    averagine_model: ms_deisotope.AveragineCache = ms_deisotope.AveragineCache(
+        averagine_map[averagine])
     averagine_model.populate(truncate_after=0.95)
     scan_index = 0
 
@@ -212,7 +226,8 @@ def main(input_path, output_path, lockmass_config, start_time=0, end_time=None, 
     else:
         end_index = float('inf')
 
-    strategy = make_iterator(reader, start_index=start_index, stop_index=end_index)
+    strategy = make_iterator(reader, start_index=start_index,
+                             stop_index=end_index, lock_mass_function=lockmass_function)
     writer.add_file_contents("MS1 spectrum")
     writer.add_file_contents("MSn spectrum")
     writer.add_data_processing(writer.build_processing_method())
@@ -228,37 +243,57 @@ def main(input_path, output_path, lockmass_config, start_time=0, end_time=None, 
                 child = children[0]
             else:
                 break
-            logger.info("Begin Memory RSS: %s", proc.memory_percent())
-            logger.info("Parent Frame: %s", parent)
-            parent.extract_features()
-            logger.info("Parent Frame Contains %d raw features", len(parent.features))
-            parent.deconvolute_features(averagine_model,
-                                        scorer=ms_deisotope.PenalizedMSDeconVFitter(5, 2),
-                                        truncate_after=0.95, minimum_intensity=minimum_intensity)
-            logger.info("Parent Frame Contains %d deconvoluted features",
-                len(parent.deconvoluted_features))
-            if len(parent.deconvoluted_features) == 0:
-                logger.info("Skipping child frames, no viable features extracted from the parent.")
-                continue
-            logger.info("Child Frame: %s", child)
-            child.extract_features()
-            logger.info("Child Frame Contains %d raw features", len(child.features))
-            child.deconvolute_features(
-                averagine_model, scorer=ms_deisotope.MSDeconVFitter(1), truncate_after=0.95,
-                minimum_intensity=minimum_intensity)
-            logger.info("Child Frame Contains %d deconvoluted features",
-                len(child.deconvoluted_features))
+            if parent:
+                logger.info("Begin Memory RSS: %s", proc.memory_percent())
+                logger.info("Parent Frame: %s", parent)
+                delta_im = np.median(np.diff(parent.ion_mobilities))
+                max_gap_size = (delta_im * 3) + (delta_im / 2)
+                parent.extract_features(max_gap_size=max_gap_size)
+                logger.info("Parent Frame Contains %d raw features",
+                            len(parent.features))
+                parent.deconvolute_features(averagine_model,
+                                            scorer=ms_deisotope.PenalizedMSDeconVFitter(
+                                                5, 2),
+                                            truncate_after=0.95, minimum_intensity=minimum_intensity,
+                                            max_gap_size=max_gap_size)
+                logger.info("Parent Frame Contains %d deconvoluted features",
+                            len(parent.deconvoluted_features))
+                if len(parent.deconvoluted_features) == 0:
+                    logger.info(
+                        "Skipping child frames, no viable features extracted from the parent.")
+                    continue
+                logger.info("Child Frame: %s", child)
+
+            for child in children:
+                delta_im = np.median(np.diff(parent.ion_mobilities))
+                max_gap_size = (delta_im * 3) + (delta_im / 2)
+                child.extract_features(max_gap_size=max_gap_size)
+                logger.info("Child Frame Contains %d raw features",
+                            len(child.features))
+                child.deconvolute_features(
+                    averagine_model, scorer=ms_deisotope.MSDeconVFitter(1), truncate_after=0.95,
+                    minimum_intensity=minimum_intensity, max_gap_size=max_gap_size)
+                logger.info("Child Frame Contains %d deconvoluted features",
+                            len(child.deconvoluted_features))
 
             if no_product_splitting:
-                writer.save_scan(parent)
-                writer.save_scan(child)
+                if parent:
+                    writer.save_scan(parent)
+                for child in children:
+                    writer.save_scan(child)
             else:
-                prec_prod_rels = connect_products_to_precursors(parent, child)
-                scan_bunch, scan_index = frame_pair_to_scan_bunch(parent, child, prec_prod_rels, scan_index)
-                logger.info("Produced %d Pseudo-MS2s", len(scan_bunch.products))
-                writer.save(scan_bunch)
+                if parent:
+                    for child in children:
+                        prec_prod_rels = connect_products_to_precursors(
+                            parent, child)
+                        scan_bunch, scan_index = frame_pair_to_scan_bunch(
+                            parent, child, prec_prod_rels, scan_index)
+                        logger.info("Produced %d Pseudo-MS2s",
+                                    len(scan_bunch.products))
+                        writer.save(scan_bunch)
             logger.info("End Memory RSS: %s", proc.memory_percent())
 
 
 if __name__ == "__main__":
+    register_debug_hook()
     main.main()
