f66be5dbcf194e5141aa30d3984415afe7fe0f5a
Joshua Klein <mobiusklein@gmail.com>
2023-05-12 08:09:04 -0400

    Update macos platform version

--- examples/split_coisolation.py
+++ examples/split_coisolation.py
@@ -9,24 +9,29 @@ from ms_deisotope.task.log_utils import init_logging, logger
 def main(inpath: os.PathLike, outpath: os.PathLike):
     reader = ProcessedMzMLLoader(inpath)
     init_logging()
-    j = 0
+    scan_count = 0
+    split_count = 0
+    logger.info("Reading spectra from %r", inpath)
+    logger.info("Writing spectra to %r", outpath)
     with MzMLSerializer(outpath, int(len(reader) * 1.5)) as writer:
         writer.copy_metadata_from(reader)
         for bunch_i, (precursor, products) in enumerate(reader):
-            if bunch_i % 5000 == 0 and bunch_i:
-                logger.info("... Handled %d/%d spectra (%0.2f%%); Wrote %d spectra", precursor.index, len(reader), precursor.index / len(reader) * 100.0, j)
+            if bunch_i % 1000 == 0 and bunch_i:
+                logger.info("... Handled %d/%d spectra (%0.2f%%); Wrote %d spectra (%d split)", precursor.index,
+                            len(reader), precursor.index / len(reader) * 100.0, scan_count, split_count)
             writer.save(precursor)
-            j += 1
+            scan_count += 1
             product: ProcessedScan
             for product in products:
                 writer.save(product)
-                j += 1
+                scan_count += 1
                 for isolation_j, coisolation in enumerate(product.precursor_information.split_coisolations()[1:], 1):
                     dup = product.clone()
                     dup.precursor_information = coisolation
                     dup.id += f".{isolation_j}"
                     writer.save(dup)
-                    j += 1
+                    scan_count += 1
+                    split_count += 1
 
 
 if __name__ == "__main__":
